21763056adb91da9605afea44c9f11890c497472
sanjivvenkatram <40864406+sanjivvenkatram@users.noreply.github.com>
2018-08-25 02:32:56 +0800

    Update rpi_ds18b20_to_pbi.py

--- rpi_ds18b20_to_pbi.py
+++ rpi_ds18b20_to_pbi.py
@@ -11,13 +11,16 @@ from datetime import datetime
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
+# Initialize
 I = 0
+# Final count
 F = 10000 
+# Inout to time.sleep function
 C = 0
 
 while I <= F:
 
-    file = open('/sys/bus/w1/devices/28-0416c4e2ecff/w1_slave')
+    file = open('/sys/bus/w1/devices/"your device number: typically starts with 28-...Note, dont need quotes"/w1_slave')
     filecontent = file.read()
     file.close()
 
@@ -29,7 +32,7 @@ while I <= F:
     #REST API endpoint, this will be given/displayed when you create an API streaming dataset in Power BI Service
     #Will be of the format "https://api.powerbi.com/beta/<tenant ID>/<datasets>/<dataset ID>/row?key = <key ID>
 
-    REST_API_URL = "https://api.powerbi.com/beta/4d690ea3-a04b-43bc-bffe-0423efe49b0e/datasets/3f672732-0056-4395-90fa-195551fa3277/rows?key=Loa9zSGoCE5HfjPvuwMKWR4c6VHFLIf8CceQUxnlrQVQZobHU2IcLxVOXsc3haEdkaSC0JCZIR%2F1Ftc0i8qKvQ%3D%3D"
+    REST_API_URL = "Input here & keep the quotes"
 
 
     print (t)                
